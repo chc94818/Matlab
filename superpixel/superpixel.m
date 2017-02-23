6a94e423e952096e4828cb9e547718936632f518
lab031_136-PC\lab031_136 <ts00140421@gmail.com>
2017-02-23 18:29:20 +0800

    learing code

---
+++ superpixel/superpixel.m
@@ -0,0 +1,22 @@
+clc;
+clear;
+A = imread('cat.jpg');
+[L,N] = superpixels(A,500);
+figure
+BW = boundarymask(L);
+imshow(imoverlay(A,BW,'cyan'),'InitialMagnification',67)
+outputImage = zeros(size(A),'like',A);
+idx = label2idx(L);
+numRows = size(A,1);
+numCols = size(A,2);
+for labelVal = 1:N
+    redIdx = idx{labelVal};
+    greenIdx = idx{labelVal}+numRows*numCols;
+    blueIdx = idx{labelVal}+2*numRows*numCols;
+    outputImage(redIdx) = mean(A(redIdx));
+    outputImage(greenIdx) = mean(A(greenIdx));
+    outputImage(blueIdx) = mean(A(blueIdx));
+end
+
+figure
+imshow(outputImage,'InitialMagnification',67)
